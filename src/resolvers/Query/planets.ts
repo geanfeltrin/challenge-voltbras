a3bfa38dceca0f8e6675496331834e59cb4854fc
Gean Feltrin <gean_costa@hotmail.com>
2020-11-06 18:10:25 -0300

    feature get more data

--- src/resolvers/Query/planets.ts
+++ src/resolvers/Query/planets.ts
@@ -1,8 +1,31 @@
-export default {
-  suitablePlanets: async (_source, { page }, { dataSources }) => {
-    const data = await dataSources.planetsApi.getPlanets(page)
+import { valueFromAST } from 'graphql'
 
-    const filterIsPossibleToInstallStation = data.results
+export default {
+  suitablePlanets: async (_source, { page }, { dataSources, prisma }) => {
+    let data = []
+    if (page) {
+      const d = await dataSources.planetsApi.getPlanets(page)
+      data.push(...d.results)
+    } else {
+      const promises = [1, 2, 3, 4, 5, 6, 7, 8].flatMap(async (item) => {
+        const d = await dataSources.planetsApi.getPlanets(item)
+        return d
+      })
+      const dataSource = await Promise.all(promises)
+      data.push(...dataSource.flatMap((value) => value.results))
+    }
+
+    const dataStation = await prisma.station.findMany()
+
+    const formatStation = dataStation.map((value) => ({
+      id: value.id,
+      name_station: value.name_station,
+      name: value.name_planet,
+      mass: value.mass,
+      hasStation: true,
+    }))
+
+    const filterIsPossibleToInstallStation = data
       .filter((item) => item.mass?.value > 25)
       .map((element) => ({
         name: element.name,
@@ -10,6 +33,11 @@ export default {
         hasStation: false,
       }))
 
-    return filterIsPossibleToInstallStation
+    const formatHasStation = [
+      ...filterIsPossibleToInstallStation,
+      ...formatStation,
+    ]
+
+    return formatHasStation
   },
 }
