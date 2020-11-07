f219f0b0cabc8226a55e9020fca6cc3282dc9aa4
Gean Feltrin <gean_costa@hotmail.com>
2020-11-07 15:34:59 -0300

    Add instace prisma

--- src/resolvers/Mutation/installStation.ts
+++ src/resolvers/Mutation/installStation.ts
@@ -1,22 +1,25 @@
-import { Context } from '../../context/index'
+import { PrismaClient } from '@prisma/client';
+
 type DataProps = {
   data: {
-    nameStation: string
-    namePlanet: string
-    mass: number
-  }
-}
+    nameStation: string;
+    namePlanet: string;
+    mass: number;
+  };
+};
+
+const prisma = new PrismaClient();
 
 export default {
-  installStation: async (_: unknown, { data }: DataProps, context: Context) => {
-    const response = await context.prisma.station.create({
+  installStation: async (_: unknown, { data }: DataProps) => {
+    const response = await prisma.station.create({
       data: {
         name_station: data.nameStation,
         name_planet: data.namePlanet,
         mass: data.mass,
       },
-    })
+    });
 
-    return response
+    return response;
   },
-}
+};
--- src/resolvers/Query/planets.ts
+++ src/resolvers/Query/planets.ts
@@ -1,60 +1,63 @@
-import { Context } from '../../context/index'
+import { PrismaClient } from '@prisma/client';
 
-type contextProps = { dataSources: any; context: Context }
-type argsProps = { page: Number }
+type argsProps = { page: number };
+
+interface ContextProps {
+  dataSources: any;
+}
+
+const prisma = new PrismaClient();
 
 export default {
-  suitablePlanets: async (
-    _: unknown,
-    args: argsProps,
-    { dataSources, context }: contextProps,
-  ) => {
-    let data = []
-    const arrayPage = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    const { page } = args
+  suitablePlanets: async (_: unknown, args: argsProps, ctx: ContextProps) => {
+    let data = [];
+    const arrayPage = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const { page } = args;
     try {
       if (page) {
-        const dataWithPage = await dataSources.planetsApi.getPlanets(page)
-        data.push(...dataWithPage.results)
+        const dataWithPage = await ctx.dataSources.planetAPI.getPlanets(page);
+        data.push(...dataWithPage.results);
       } else {
-        const promises = arrayPage.flatMap(async (item) => {
-          const dataWithoutPage = await dataSources.planetsApi.getPlanets(item)
-          return dataWithoutPage
-        })
-        const dataSource = await Promise.all(promises)
-        data.push(...dataSource.flatMap((value) => value.results))
+        const promises = arrayPage.flatMap(async item => {
+          const dataWithoutPage = await ctx.dataSources.planetAPI.getPlanets(
+            item,
+          );
+          return dataWithoutPage;
+        });
+        const dataSource = await Promise.all(promises);
+        data.push(...dataSource.flatMap(value => value.results));
       }
 
       const filterIsPossibleToInstallStation = data
-        .filter((item) => item.mass?.value > 25)
-        .map((element) => ({
+        .filter(item => item.mass?.value > 25)
+        .map(element => ({
           name: element.name,
           mass: element.mass.value,
           hasStation: false,
-        }))
+        }));
 
-      const dataStation = await context.prisma.station.findMany()
+      const dataStation = await prisma.station.findMany();
 
       if (dataStation) {
         for (let stations of dataStation) {
           for (let isPossibleToInstallStation of filterIsPossibleToInstallStation) {
             if (stations.name_planet === isPossibleToInstallStation.name) {
-              isPossibleToInstallStation.hasStation = true
+              isPossibleToInstallStation.hasStation = true;
             }
           }
         }
 
-        const formatHasStation = [...filterIsPossibleToInstallStation]
+        const formatHasStation = [...filterIsPossibleToInstallStation];
         const result = formatHasStation.sort(
           (a, b) => Number(b.hasStation) - Number(a.hasStation),
-        )
+        );
 
-        return result
+        return result;
       } else {
-        return filterIsPossibleToInstallStation
+        return filterIsPossibleToInstallStation;
       }
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   },
-}
+};
