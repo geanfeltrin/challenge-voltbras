e316bbb10f67ddb0be1295705b0b2d7b57602a8c
Gean Feltrin <gean_costa@hotmail.com>
2020-11-08 13:57:23 -0300

    Rename mutation

---
+++ src/resolvers/Mutation/station.ts
@@ -0,0 +1,39 @@
+import { PrismaClient } from '@prisma/client';
+import { Context } from '../../context/db';
+
+type DataProps = {
+  data: {
+    nameStation: string;
+    namePlanet: string;
+    mass: number;
+  };
+};
+
+interface ctxProps {
+  db: Context;
+}
+
+export default {
+  installStation: async (_: unknown, { data }: DataProps, ctx: Context) => {
+    const { db } = ctx;
+    try {
+      const response = await db.station.create({
+        data: {
+          name_station: data.nameStation,
+          name_planet: data.namePlanet,
+          mass: data.mass,
+        },
+      });
+
+      const result = {
+        id: response.id,
+        namePlanet: response.name_planet,
+        nameStation: response.name_station,
+        mass: response.mass,
+      };
+      return result;
+    } catch (error) {
+      throw new Error('This planet already been registered, try other planet.');
+    }
+  },
+};
