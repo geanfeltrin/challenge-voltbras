04d6906c290fe649bec796360015b11dfa571814
Gean Feltrin <gean_costa@hotmail.com>
2020-11-08 14:00:25 -0300

    Add station test

---
+++ src/__tests__/station.spec.ts
@@ -0,0 +1,34 @@
+import station from '../resolvers/Mutation/station';
+import { createMockContext, MockContext } from './mockContext';
+import { Context } from '../context/db';
+
+describe('Mutation-Station', () => {
+  let mockCtx: MockContext;
+  let ctx: Context;
+
+  beforeEach(() => {
+    mockCtx = createMockContext();
+    ctx = (mockCtx as unknown) as Context;
+  });
+  it('should be able to create a new station', async () => {
+    const data = {
+      nameStation: 'station',
+      namePlanet: '01',
+      mass: 60,
+    };
+
+    mockCtx.db.station.create.mockResolvedValue({
+      id: 5,
+      name_station: data.nameStation,
+      name_planet: data.namePlanet,
+      mass: data.mass,
+    });
+
+    const response = await station.installStation(null, { data }, ctx);
+
+    expect(response).toEqual({ ...data, id: 5 });
+  });
+
+  it('should return error if data was incorrect', async () => {});
+  it('should return error if station already been registred', async () => {});
+});
