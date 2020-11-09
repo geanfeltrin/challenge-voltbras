7720214bf530010e423e7087a1e05b18a47ad972
Gean Feltrin <gean_costa@hotmail.com>
2020-11-09 11:07:13 -0300

    Add orderBy sortElements

--- src/utils/sortElements.ts
+++ src/utils/sortElements.ts
@@ -1,8 +1,20 @@
-export function sortElements(arr: Array<any>, key: string) {
+export function sortElements(
+  arr: Array<any>,
+  key: string,
+  orderBy: 'desc' | 'asc',
+) {
+  if (arr.length < 1) {
+    return [];
+  }
   if (!(key in arr[0])) {
     return null;
   }
-  const result = arr.sort((a, b) => Number(b[key]) - Number(a[key]));
 
-  return result;
+  if (orderBy === 'asc') {
+    const result = arr.sort((a, b) => Number(b[key]) - Number(a[key]));
+    return result;
+  } else if (orderBy === 'desc') {
+    const result = arr.sort((a, b) => Number(a[key]) - Number(b[key]));
+    return result;
+  }
 }
