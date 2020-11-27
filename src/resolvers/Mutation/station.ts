3a18725e41be2f3285f436b3cb7288e3290f2530
Gean Costa Feltrin <32302438+geanfeltrin@users.noreply.github.com>
2020-11-26 23:27:00 -0300

    Remove error message

--- src/resolvers/Mutation/station.ts
+++ src/resolvers/Mutation/station.ts
@@ -8,19 +8,12 @@ type DataProps = {
 
 export default {
   installStation: async (_: unknown, { data }: DataProps, { db }: Context) => {
-    try {
+    
       const response = await db.station.create({
         data: {
           name: data.name,
         },
       });
-
-      return response;
-    } catch (error) {
-      // novamente não acho muito massa formatar o erro aqui
-      throw new Error(
-        'Could not possible to install station, the name must be unique.',
-      );
-    }
+      return response;   
   },
 };
